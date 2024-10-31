9a81ad3bc161abbcabc5b47d226aa987cd7cd057
Ali Kemal Çalak <kemalcalak@gmail.com>
2024-10-31 21:06:19 +0300

    add admin and shopping components, layouts, and routing

---
+++ client/src/pages/shopping-view/checkout.jsx
@@ -0,0 +1,9 @@
+function ShoppingCheckout() {
+  return (
+    <div>
+      <h1>Shopping Checkout</h1>
+    </div>
+  );
+}
+
+export default ShoppingCheckout;
---
+++ client/src/pages/shopping-view/home.jsx
@@ -0,0 +1,9 @@
+function ShoppingHome() {
+    return ( 
+        <div>
+            <h1>Shopping Home</h1>
+        </div>
+     );
+}
+
+export default ShoppingHome;
