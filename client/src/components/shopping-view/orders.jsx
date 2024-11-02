5ee650855a212e189759f1c8ac523c6b00d8892f
Ali Kemal Çalak <kemalcalak@gmail.com>
2024-11-02 15:40:09 +0300

    update currency display to Turkish Lira (TL) in order and cart components

--- client/src/components/shopping-view/orders.jsx
+++ client/src/components/shopping-view/orders.jsx
@@ -76,7 +76,7 @@ function ShoppingOrders() {
                         {orderItem?.orderStatus}
                       </Badge>
                     </TableCell>
-                    <TableCell>${orderItem?.totalAmount}</TableCell>
+                    <TableCell>{orderItem?.totalAmount}TL</TableCell>
                     <TableCell>
                       <Dialog
                         open={openDetailsDialog}
